c9c490c552984e5c372aa4eaad53a2a95d59f11b
Felipe Alpino <felipealpino1@gmail.com>
2021-03-22 20:23:42 -0300

    criando header - estrutura e logo

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import {HeaderArea} from './styled'
+import {Link} from 'react-router-dom'
+
 
 const Header = () => {
     return (<>
         <HeaderArea>
-            OI EU SOU O HEADER
+            <div className="container">
+                <div className="logo">
+                    <Link to="/">
+                        <span className="logo1">O</span>
+                        <span className="logo2">L</span>
+                        <span className="logo3">X</span>
+                    </Link>
+                </div>
+            </div>
         </HeaderArea>
     </>)
 }
--- src/components/partials/Header/styled.js
+++ src/components/partials/Header/styled.js
@@ -2,6 +2,35 @@ import styled from 'styled-components'
 
 
 export const HeaderArea = styled.div`
-    height:100px;
-    background-color:grey;
+    background-color:#FFF;
+    height:60px;
+    border-bottom:1px solid #CCC;
+
+.container{
+    max-width:1000px;
+    margin:auto;
+    display:flex;
+}
+
+a{
+    text-decoration:none;
+}
+
+.logo{
+    flex:1;
+    display:flex;
+    align-items:center;
+    height:60px;
+    
+    .logo1,
+    .logo2,
+    .logo3{
+        font-size:27px;
+        font-weight:bold;
+    }
+    .logo1{color:#FF0000}
+    .logo2{color:#00FF00}
+    .logo3{color:#0000FF}
+}
+
 `
