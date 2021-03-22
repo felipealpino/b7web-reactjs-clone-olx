cf4972a4b9dcbcf7179be78f02695f9103c76c1e
Felipe Alpino <felipealpino1@gmail.com>
2021-03-22 18:21:46 -0300

    Criando App.css padrao

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
 
+import  './App.css'
+
 import Routes from './Routes'
 
 import { Template } from './components/MainComponents'
